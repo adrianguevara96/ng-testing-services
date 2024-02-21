6418713d7615e50ecb76f28cdd4bb2ab1cfcd6ad
Adrian Guevara <adrianjga96@gmail.com>
2024-02-21 07:11:34 -0400

    feat: components with Input

--- testing-services/src/app/components/person/person.component.spec.ts
+++ testing-services/src/app/components/person/person.component.spec.ts
@@ -3,8 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonComponent } from './person.component';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
 
-fdescribe('PersonComponent', () => {
+describe('PersonComponent', () => {
   let component: PersonComponent;
   let fixture: ComponentFixture<PersonComponent>;
 
@@ -21,14 +22,27 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <p> with "Soy un parrafo"', () => {
+  it('should the name be "Adrian"', () => {
+    component.person = new Person('Adrian', 'Guevara', 28, 80, 1.76);
+    expect(component.person.name).toEqual('Adrian');
+  })
+
+  it('should have <p> with "Mi altura es {{ person.height }} "', () => {
+    component.person = new Person('Adrian', 'Guevara', 28, 80, 1.76);
+    // arrange
     // elemento que se renderiza
     const personElement: HTMLElement = fixture.nativeElement;
     const p = personElement.querySelector('p');
-    expect(p?.textContent).toEqual('Soy un parrafo');
+    // act
+    fixture.detectChanges();
+
+    // assert
+    expect(p?.textContent).toEqual(`Mi altura es ${component.person.height}`);
   });
 
-  it('should have <p> with "Soy un parrafo" with debugElement && byCss', () => {
+  it('should have <p> with "Mi altura es {{ person.height }}" with debugElement && byCss', () => {
+    component.person = new Person('Adrian', 'Guevara', 28, 80, 1.76);
+
     // para aplicar server side rendering, mejorar el rendimiento
     // buena practica, crear el debug element y de el partir
     const personDebug: DebugElement = fixture.debugElement;
@@ -36,13 +50,24 @@ fdescribe('PersonComponent', () => {
     const pDebug: DebugElement = personDebug.query(By.css('p'));
     // elemento que se renderiza
     const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement.textContent).toEqual('Soy un parrafo');
+
+    fixture.detectChanges();
+
+    expect(pElement.textContent).toEqual(`Mi altura es ${component.person.height}`);
   });
 
-  it('should have <h3> with "Hola, PersonComponent" with debugElement && byCss', () => {
+  it('should have <h3> with "Hola, {{ person.name }}" with debugElement && byCss', () => {
+    component.person = new Person('Adrian', 'Guevara', 28, 80, 1.76);
+
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3Element: HTMLElement = h3Debug.nativeElement;
-    expect(h3Element.textContent).toEqual('Hola, PersonComponent');
+
+    fixture.detectChanges();
+
+    // to equal
+    expect(h3Element.textContent).toEqual(`Hola, ${component.person.name}`);
+    // to contain
+    expect(h3Element.textContent).toContain(`Hola, ${component.person.name}`);
   });
 });
