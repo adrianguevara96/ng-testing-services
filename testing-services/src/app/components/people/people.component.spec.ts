a2c8d685863d159f93785f796cf5c7be2f9c262c
Adrian Guevara <adrianjga96@gmail.com>
2024-09-24 12:32:28 -0400

    feat: challenge

--- testing-services/src/app/components/people/people.component.spec.ts
+++ testing-services/src/app/components/people/people.component.spec.ts
@@ -2,8 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
 import { PersonComponent } from '../person/person.component';
+import { Person } from 'src/app/models/person.model';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-describe('PeopleComponent', () => {
+fdescribe('PeopleComponent', () => {
   let component: PeopleComponent;
   let fixture: ComponentFixture<PeopleComponent>;
 
@@ -18,5 +21,43 @@ describe('PeopleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list app-person components', () => {
+    // arrange
+    component.people = [
+      new Person('Jose', 'Perez', 23, 1, 1),
+      new Person('Valentina', 'Perez', 12, 1, 1),
+      new Person('Santiago', 'Perez', 16, 1, 1)
+    ];
+    fixture.detectChanges();
+
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+    // act
+    // assert
+    expect(debugElement.length).toEqual(3);
+  });
+
+  // challenge
+  it('should have a list app-person components & Valentina person selected', () => {
+    // arrange
+    component.people = [
+      new Person('Jose', 'Perez', 23, 1, 1),
+      new Person('Valentina', 'Perez', 12, 1, 1),
+      new Person('Santiago', 'Perez', 16, 1, 1)
+    ];
+    fixture.detectChanges();
+
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person .btn-choose'));
+    // act
+    debugElement[1].triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const liDebugElement: DebugElement[] = fixture.debugElement.queryAll(By.css('li'));
+    const liElement: HTMLElement = liDebugElement[0].nativeElement;
+    // assert
+    expect(debugElement.length).toEqual(3);
+    expect(liElement.textContent).toContain(component.people[1].name);
+    expect(component.selectedPerson?.lastName).toEqual(component.people[1].lastName)
   });
 });
--- testing-services/src/app/components/people/people.component.ts
+++ testing-services/src/app/components/people/people.component.ts
@@ -9,4 +9,13 @@ import { Person } from 'src/app/models/person.model';
 export class PeopleComponent {
 
   person: Person = new Person('Adrian', 'Guevara', 28, 86, 1.76);
+  people: Person[] = [
+    new Person('Adrian', 'Guevara', 28, 86, 1.76),
+    new Person('Anyami', 'Cornieles', 29, 70, 1.62)
+  ];
+  selectedPerson: Person | null = null;
+
+  choose(person: Person) {
+    this.selectedPerson = person;
+  }
 }
