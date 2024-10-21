1436c5baf905ba4cc5d9d557230c5bb914fef153
Adrian Guevara <adrianjga96@gmail.com>
2024-10-21 15:04:14 -0400

    feat: testing pipe

--- testing-services/src/app/pipes/reverse.pipe.spec.ts
+++ testing-services/src/app/pipes/reverse.pipe.spec.ts
@@ -1,6 +1,10 @@
+import { Component, DebugElement } from '@angular/core';
 import { ReversePipe } from './reverse.pipe';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-fdescribe('ReversePipe', () => {
+describe('ReversePipe', () => {
   it('create an instance', () => {
     const pipe = new ReversePipe();
     expect(pipe).toBeTruthy();
@@ -20,3 +24,58 @@ fdescribe('ReversePipe', () => {
     expect(res).toEqual("arepera");
   })
 });
+
+@Component({
+  template: `
+  <h5>{{ 'amor' | reverse }}</h5>
+  <input type="text" [(ngModel)]="text">
+  <p>{{ text | reverse }}</p>
+  `
+})
+class HostComponent {
+  text = '';
+}
+
+fdescribe('HostComponent with ReversePipe', () => {
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+
+  beforeEach( async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HostComponent, ReversePipe],
+      imports: [FormsModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('shoudl be create', () => {
+    expect(component).toBeTruthy();
+  })
+
+  it('should the h5 be "roma"', () => {
+    const h5DebugElement: DebugElement = fixture.debugElement.query(By.css('h5'));
+    const h5Element: HTMLElement = h5DebugElement.nativeElement;
+
+    expect(h5Element.textContent).toEqual("roma");
+  })
+
+  it('should apply reverse pipe when typing in the input', () => {
+    const inputDebugElement: DebugElement = fixture.debugElement.query(By.css('input'));
+    const inputElement: HTMLInputElement = inputDebugElement.nativeElement;
+
+    const pDebugElement: DebugElement = fixture.debugElement.query(By.css('p'));
+    const pElement: HTMLElement = pDebugElement.nativeElement;
+
+    expect(pElement.textContent).toEqual('');
+
+    inputElement.value = 'arepera';
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    expect(pElement.textContent).toEqual("arepera");
+  })
+})
